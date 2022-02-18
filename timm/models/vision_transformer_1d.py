576cc8400c9ace63988b0a6f8ef85a087c68762a
Nicolas Burrus <nicolas@burrus.name>
2022-02-18 09:05:52 +0100

    Add a disabled hack to analyze the attention maps.

--- timm/models/vision_transformer_1d.py
+++ timm/models/vision_transformer_1d.py
@@ -221,6 +221,8 @@ class Attention(nn.Module):
 
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
+        # Hack to analyze the attention afterwards.
+        # self.last_attn = attn.clone()
         attn = self.attn_drop(attn)
 
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
